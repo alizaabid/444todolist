23977093595e6fd2b64131200c466854d7af7d1b
alizaabid <155987307+alizaabid@users.noreply.github.com>
2024-04-20 20:53:32 +0500

    push

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([{
+            name: "task",
+            type: "input",
+            message: "what you want to add in todo list?"
+        },
+        { name: "addMore",
+            type: "confirm",
+            message: "do you want to add more ?",
+            default: "false" }]);
+    todos.push(addTask.task);
+    condition = addTask.addMore;
+    console.log(todos);
+}
